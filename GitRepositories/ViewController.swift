442f52841b0a9f76b515cf0f82f3ab03df030ec8
Appi <iphonedev@appi.com.br>
2018-10-01 21:24:50 -0300

    Initial Commit

---
+++ GitRepositories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitRepositories
+//
+//  Created by Luiz Fernando on 01/10/18.
+//  Copyright © 2018 Luiz Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
